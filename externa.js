a6435d1334ea4fbc04886e461821f543b375881d
Inzamamul Shareef <inzushareef44@gmail.com>
2022-03-27 23:53:33 +0530

    Add the return statement in place of Console.log

--- externa.js
+++ externa.js
@@ -2,6 +2,6 @@ function ComputerPlay(){
     const arr = ['Rock','Paper','Scissors'];
 
     let index = Math.floor(Math.random()*3);
-    console.log(index);
-    
+    //console.log(index);
+    return arr[index];  
 }
