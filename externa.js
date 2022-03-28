e4f17f6d56de3c463765d26b2d72067dba7c8c46
Inzamamul Shareef <inzushareef44@gmail.com>
2022-03-28 13:59:24 +0530

    Make some changes again that makes the game more interactive
    Here in the updated version of the code we can see that we are counting the scores and displaying them after each round using alert. And at the end of the game we announce who has won the game. Here in the rounds that same selection is drawn we don't give and points and move onto the other rounds as usual.

--- externa.js
+++ externa.js
@@ -1,3 +1,8 @@
+
+let Playerscore = 0;
+let Computerscore = 0;
+
+
 function Captalize(str){
     str = str.toLowerCase();
     const str1 = str.slice(1);
@@ -24,21 +29,36 @@ function playRound(ComputerSelection,PlayerSelection){
     else if((ComputerSelection == "Rock" && PlayerSelection == "Scissors") || (ComputerSelection == "Paper" && PlayerSelection == "Rock") || (ComputerSelection == "Scissors" && PlayerSelection == "Paper")){
         //console.log("You loose " + ComputerSelection +" beats "+ PlayerSelection);
         result = ("You loose " + ComputerSelection +" beats "+ PlayerSelection);
+        Computerscore++;
     }
     else {
         //console.log("YOU WIN !!" + PlayerSelection +" beats "+ ComputerSelection );
-        result = ("YOU WIN !!" + PlayerSelection +" beats "+ ComputerSelection);
+        result = ("YOU WIN !! " + PlayerSelection +" beats "+ ComputerSelection);
+        Playerscore++;
     }
     return result;
 }
 
 function game(number){
-    for (let i=0 ; i<number; i++){
+    for (let i = 0; i < number; i++){
         let PlayerSelection = prompt("Please Choose your choice from (Rock,Paper,Scissors) :");
 
-        alert((playRound(ComputerPlay(),PlayerSelection)));
+        alert((playRound(ComputerPlay(),PlayerSelection)) + " Socre as of round " + (i+1) + " are Computer: "+ 
+        Computerscore + " and "+ "Player: "+ Playerscore);
     }
 }
-let number = prompt("Please enter how many rounds of game is this : ");
 
+
+let number = prompt("Please enter how many rounds of game is this : ");
+//console.log(number)
 game(number);
+
+if (Playerscore > Computerscore){
+    alert("Well You think Computers are Smart ? Nah We make then smart. Congragulations You WIN the Game. You ROCK!");
+}
+else if (Playerscore < Computerscore){
+    alert(" Computers aren't meant to be humiliated by human. Well played! Better luck Next time! ");
+}
+else{
+    alert("You didn't let a computer beat you. Probably that your Achievement.");
+}
