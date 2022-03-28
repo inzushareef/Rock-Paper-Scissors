3f7735c0538789f54bba2145a37996bc875b5903
Inzamamul Shareef <inzushareef44@gmail.com>
2022-03-28 12:01:15 +0530

    Add the PlayRoud Function which is the core of this project

--- externa.js
+++ externa.js
@@ -1,3 +1,10 @@
+function Captalize(str){
+    str = str.toLowerCase();
+    const str1 = str.slice(1);
+    return str.charAt(0).toUpperCase()+ str1;
+
+}
+
 function ComputerPlay(){
     const arr = ['Rock','Paper','Scissors'];
 
@@ -5,3 +12,19 @@ function ComputerPlay(){
     //console.log(index);
     return arr[index];  
 }
+
+function playRound(ComputerSelection,PlayerSelection){
+    PlayerSelection = Captalize(PlayerSelection);
+    if (ComputerSelection == PlayerSelection){
+        console.log("Both the selections are same. Please Try again!");
+
+    }
+    else if((ComputerSelection == "Rock" && PlayerSelection == "Scissors") || (ComputerSelection == "Paper" && PlayerSelection == "Rock") || (ComputerSelection == "Scissors" && PlayerSelection == "Paper")){
+        console.log("You loose " + ComputerSelection +" beats "+ PlayerSelection);
+    
+    }
+    else {
+        console.log("YOU WIN !!" + PlayerSelection +" beats "+ ComputerSelection );
+    }
+}
+
