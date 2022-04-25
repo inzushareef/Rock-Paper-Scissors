d8e37c10eed25dd4b12b74697fe3446416b94ef9
Inzamamul Shareef <inzushareef44@gmail.com>
2022-04-25 13:13:39 +0530

    Add event listeners to the buttons After so many tries finally able to add an even listeners that allows you to play with the computer and it alets the results on the screen. And a lot more changes to come stay tuned

--- externa.js
+++ externa.js
@@ -13,6 +13,7 @@ function Captalize(str){
 function ComputerPlay(){
     const arr = ['Rock','Paper','Scissors'];
 
+    //generates Random number between 0 , 1, 2
     let index = Math.floor(Math.random()*3);
     //console.log(index);
     return arr[index];  
@@ -38,27 +39,52 @@ function playRound(ComputerSelection,PlayerSelection){
     }
     return result;
 }
+//Logic the plays the round five time. Best of the given number of round wins.
+// function game(number){
+//     for (let i = 0; i < number; i++){
+//         let PlayerSelection = prompt("Please Choose your choice from (Rock,Paper,Scissors) :");
 
-function game(number){
-    for (let i = 0; i < number; i++){
-        let PlayerSelection = prompt("Please Choose your choice from (Rock,Paper,Scissors) :");
-
-        alert((playRound(ComputerPlay(),PlayerSelection)) + " Socre as of round " + (i+1) + " are Computer: "+ 
-        Computerscore + " and "+ "Player: "+ Playerscore);
-    }
-}
+//         alert((playRound(ComputerPlay(),PlayerSelection)) + " Socre as of round " + (i+1) + " are Computer: "+ 
+//         Computerscore + " and "+ "Player: "+ Playerscore);
+//     }
+// }
 
 
-let number = prompt("Please enter how many rounds of game is this : ");
-//console.log(number)
-game(number);
+// let number = prompt("Please enter how many rounds of game do you wish to play : ");
+// //console.log(number)
+// game(number);
 
-if (Playerscore > Computerscore){
-    alert("Well You think Computers are Smart ? Nah We make then smart. Congragulations You WIN the Game. You ROCK!");
-}
-else if (Playerscore < Computerscore){
-    alert(" Computers aren't meant to be humiliated by human. Well played! Better luck Next time! ");
-}
-else{
-    alert("You didn't let a computer beat you. Probably that your Achievement.");
-}
+// if (Playerscore > Computerscore){
+//     alert("Well You think Computers are Smart ? Nah We make then smart. Congragulations You WIN the Game. You ROCK!");
+// }
+// else if (Playerscore < Computerscore){
+//     alert(" Computers aren't meant to be humiliated by human. Well played! Better luck Next time! ");
+// }
+// else{
+//     alert("You didn't let a computer beat you. Probably that's your Achievement. Try Playing the game !!");
+// }
+
+
+// const btn = document.querySelector('#Rock');
+// //console.log(btn.id);
+// btn.addEventListener('click', function (e){
+//         console.log(e);
+//      });
+//btn.onclick = () => alert("Hey using JavaScript");
+
+// const btn = document.querySelector('#Rock');
+// console.log(btn);
+// btn.addEventListener('click', function (e){
+//     console.log(e);
+// });
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener('click',() => {
+        console.log(button.id);
+        const result = playRound(ComputerPlay(),button.id);
+        alert(result);
+
+    });
+});
